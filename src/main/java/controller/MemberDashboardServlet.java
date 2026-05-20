1ab02b8f1048c20c817e85245b9a139b16381bbc
Nishan Rai <nishansampang9@gmail.com>
2026-05-20 11:08:29 +0545

    update memeberdashboard extend with httpservlet

--- src/main/java/controller/MemberDashboardServlet.java
+++ src/main/java/controller/MemberDashboardServlet.java
@@ -1,6 +1,28 @@
 package controller;
 
-public class MemberDashboardServlet {
-    
-    
+import dao.CommunityDao;
+import dao.CourseDao;
+import dao.EnrollmentDao;
+import model.Community;
+import model.Course;
+import model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/member/dashboard")
+public class MemberDashboardServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User currentUser = (User) req.getSession().getAttribute("currentUser");
+
+
+    }
 }
