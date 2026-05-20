8bf1febbc786802f0f056615a3ddeae92179c630
Nishan Rai <nishansampang9@gmail.com>
2026-05-20 08:55:02 +0545

    add the memeberdashboard servlet class

--- src/main/java/controller/MemberDashboardServlet.java
+++ src/main/java/controller/MemberDashboardServlet.java
@@ -1,3 +1,5 @@
+
+
 package controller;
 
 import dao.CommunityDao;
@@ -19,28 +21,11 @@ import java.util.List;
 
 @WebServlet("/member/dashboard")
 public class MemberDashboardServlet extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User currentUser = (User) req.getSession().getAttribute("currentUser");
 
-        try {
-            List<Community> communities = new CommunityDao().listAll();
-            req.setAttribute("communities", communities);
 
-            EnrollmentDao enrollmentDao = new EnrollmentDao();
-            CourseDao courseDao = new CourseDao();
-            List<Course> enrolledCourses = new ArrayList<>();
-            for (Integer courseId : enrollmentDao.listCourseIdsByUser(currentUser.getId())) {
-                Course course = courseDao.findById(courseId);
-                if (course != null) {
-                    enrolledCourses.add(course);
-                }
-            }
-            req.setAttribute("enrolledCourses", enrolledCourses);
-
-            req.getRequestDispatcher("/WEB-INF/views/member/member-dashboard.jsp").forward(req, resp);
-        } catch (SQLException e) {
-            throw new ServletException(e);
-        }
     }
 }
